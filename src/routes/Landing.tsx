3de30c4261b5d0c4430f94f0ab0c00122f947781
lalexgap <me@alexgap.ca>
2023-10-29 17:47:58 -0700

    fix typo

--- src/routes/Landing.tsx
+++ src/routes/Landing.tsx
@@ -1,7 +1,7 @@
 function Landing() {
   return (
     <div>
-      Hello I'm Alex! Check out my <a href="/resume">resume</a> or some of my
+      Hello I'm Alex! Check out my <a href="/resume">resume</a> or some of my{" "}
       <a href="/projects">projects</a>.
     </div>
   );
