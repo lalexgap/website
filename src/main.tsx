9af2baa88dfe29014369662b213cf658f8181e4c
lalexgap <me@alexgap.ca>
2023-10-29 17:47:37 -0700

    rename to landing for clarity

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Root from "./routes/Root.tsx";
+import Landing from "./routes/Landing.tsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Resume from "./routes/Resume.tsx";
@@ -8,7 +8,7 @@ import Projects from "./routes/Projects.tsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: <Landing />,
   },
   {
     path: "/resume",
---
+++ src/routes/Landing.tsx
@@ -0,0 +1,10 @@
+function Landing() {
+  return (
+    <div>
+      Hello I'm Alex! Check out my <a href="/resume">resume</a> or some of my
+      <a href="/projects">projects</a>.
+    </div>
+  );
+}
+
+export default Landing;
