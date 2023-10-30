28bef7926a2d2395c512bb543e0a0058e5214da5
lalexgap <me@alexgap.ca>
2023-10-29 17:24:56 -0700

    Wire up basic resume functionality

---
+++ src/Resume.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+
+const GITHUB_RESUME_URL =
+  "https://raw.githubusercontent.com/lalexgap/resume/main/resume.md";
+
+const GENERATED_PDF_URL = "./generated/resume.pdf";
+
+export function Resume(): React.ReactElement {
+  const [resumeMarkdown, setResumeMarkdown] = useState("");
+  useEffect(() => {
+    const fetchAndSetResumeMarkdown = async () => {
+      const response = await fetch(GITHUB_RESUME_URL);
+      setResumeMarkdown(await response.text());
+    };
+    fetchAndSetResumeMarkdown();
+  });
+
+  return (
+    <div>
+      Download a copy of my resume <a href={GENERATED_PDF_URL}>here</a>.
+      <Markdown>{resumeMarkdown}</Markdown>
+    </div>
+  );
+}
