401197c352deb6541ea36831cb505b19a8c784d4
lalexgap <me@alexgap.ca>
2023-10-31 13:36:07 -0700

    tweak landing page

--- src/routes/Landing.tsx
+++ src/routes/Landing.tsx
@@ -1,27 +1,32 @@
-
 import { Box, Typography, Link, CardMedia } from "@mui/material";
 
 function Landing() {
   return (
     <Box
- 
       display="flex"
       justifyContent="center" // Horizontally centers the content
       alignItems="center" // Vertically centers the content
       flexDirection="column"
-      marginTop={20}
     >
       <CardMedia
         component="img"
         image="alex.png"
-        sx={{ maxHeight: 385, maxWidth: 385, borderRadius: "50%" }}
+        sx={{
+          maxHeight: 385,
+          maxWidth: 385,
+          borderRadius: "50%",
+          padding: 2,
+        }}
       />
-      <Typography color="textPrimary" variant="h2" gutterBottom>
-        Hello I'm Alex, and I'm a full-stack web3 developer!
+      <Typography color="textPrimary" variant="h1" gutterBottom>
+        Alex Gap
+      </Typography>
+      <Typography color="textSecondary" variant="h2" gutterBottom>
+        full-stack web3 developer
       </Typography>
       <Typography color="textPrimary" variant="h4" gutterBottom>
-        Check out my <Link href="/resume">resume</Link> or some of my{" "}
-        <Link href="/projects">projects</Link>.
+        Check out my <Link href="/resume">resume</Link> or some of the{" "}
+        <Link href="/projects">projects</Link> I've worked on.
       </Typography>
     </Box>
   );
