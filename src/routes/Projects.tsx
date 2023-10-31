90533fd2db0801b12e6448c758bb1e337b376ca1
lalexgap <me@alexgap.ca>
2023-10-31 10:04:56 -0700

    switch to cards

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -1,6 +1,13 @@
 import Carousel from "react-material-ui-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Paper, Link, Typography, Container, CardMedia } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Link,
+  Typography,
+  Container,
+  CardMedia,
+} from "@mui/material";
 
 function Projects() {
   return (
@@ -9,29 +16,32 @@ function Projects() {
         Projects
       </Typography>{" "}
       <Carousel>
-        <Paper>
+        <Card>
           <CardMedia component="img" image="go-nitro.png" />
-          <Typography>
-            {" "}
-            <Link href="https://github.com/statechannels/go-nitro">
-              go-nitro state channels framework
-            </Link>
-            . As part of the state channels team I designed and implemented
-            various parts of our state channel framework in Go.
-          </Typography>
-        </Paper>
-        <Paper>
+          <CardContent>
+            <Typography>
+              {" "}
+              <Link href="https://github.com/statechannels/go-nitro">
+                go-nitro state channels framework
+              </Link>
+              . As part of the state channels team I designed and implemented
+              various parts of our state channel framework in Go.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card>
           <CardMedia component="img" image="bytecode-debugger.gif" />
-          <Typography>
-            {" "}
-            <Link href="https://github.com/lalexgap/bytecode-debugger">
-              EVM Bytecode Debugger
-            </Link>{" "}
-            . A little CLI tool that lets you step through EVM bytecode, to see
-            what's going on under the hood of your smart contracts.
-          </Typography>
-        </Paper>
-        <Paper>
+          <CardContent>
+            <Typography>
+              <Link href="https://github.com/lalexgap/bytecode-debugger">
+                EVM Bytecode Debugger
+              </Link>{" "}
+              . A little CLI tool that lets you step through EVM bytecode, to
+              see what's going on under the hood of your smart contracts.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card>
           <CardMedia component="img" image="ethglobal-hack.png" />
           <Typography className="legend">
             <Link href="https://ethglobal.com/showcase/scbridgeaccount-ivyas">
@@ -41,17 +51,19 @@ function Projects() {
             (ERC 4337) that also allowed instant bridging of assets between
             chains. Our entry won "Polygon — Best Public G \sadas asdas
           </Typography>
-        </Paper>
-        <Paper>
+        </Card>
+        <Card>
           <CardMedia component="img" image="web3torrent.gif" />
-          <Typography className="legend">
-            <Link href="https://web3torrent.statechannels.org/">
-              Web3Torrent
-            </Link>
-            . As part of the state channels team I helped build web3 torrent
-            that integrates state channel payments into web torrent.
-          </Typography>
-        </Paper>
+          <CardContent>
+            <Typography>
+              <Link href="https://web3torrent.statechannels.org/">
+                Web3Torrent
+              </Link>
+              . As part of the state channels team I helped build web3 torrent
+              that integrates state channel payments into web torrent.
+            </Typography>
+          </CardContent>
+        </Card>
       </Carousel>
     </Container>
   );
