3160c57ed094c5c2fd6e68d6d3969e47c1f51947
lalexgap <me@alexgap.ca>
2023-10-31 10:39:39 -0700

    Add image

--- src/routes/Landing.tsx
+++ src/routes/Landing.tsx
@@ -1,5 +1,5 @@
 import theme from "../theme";
-import { Box, Typography, Link } from "@mui/material";
+import { Box, Typography, Link, CardMedia } from "@mui/material";
 
 function Landing() {
   return (
@@ -9,7 +9,13 @@ function Landing() {
       justifyContent="center" // Horizontally centers the content
       alignItems="center" // Vertically centers the content
       height="100vh" // Takes the full height of the viewport
+      flexDirection="column"
     >
+      <CardMedia
+        component="img"
+        image="alex.png"
+        sx={{ maxHeight: 385, maxWidth: 385, borderRadius: "50%" }}
+      />
       <Typography color="textPrimary" variant="h2" gutterBottom>
         Hello I'm Alex! Check out my <Link href="/resume">resume</Link> or some
         of my <Link href="/projects">projects</Link>.
