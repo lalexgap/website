ac7c4c91774ce6513986f89ffd886a2e57296347
lalexgap <me@alexgap.ca>
2023-10-31 13:13:59 -0700

    Resume styling

--- src/routes/Resume.tsx
+++ src/routes/Resume.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Container, Link } from "@mui/material";
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 const GITHUB_RESUME_URL =
   "https://raw.githubusercontent.com/lalexgap/resume/main/resume.md";
 
@@ -15,13 +16,34 @@ function Resume(): React.ReactElement {
   });
 
   return (
-    <div>
-      <Box sx={{ padding: "1rem" }}>
-        <Button onClick={() => window.open("/resume.pdf")}>Download</Button>
-
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "1rem",
+      }}
+    >
+      <Link color="inherit" href="/resume.pdf">
+        <Button color="inherit">
+          Download a PDF copy
+          <PictureAsPdfIcon />
+        </Button>
+      </Link>
+      <Box
+        sx={{
+          padding: "1rem",
+          background: "white",
+          marginTop: "1rem",
+          borderRadius: 1,
+          fontPalette: "text.primary",
+          fontFamily: "monospace",
+          border: "5px solid rgba(0, 0, 0, 0.12)",
+        }}
+      >
         <Markdown>{resumeMarkdown}</Markdown>
       </Box>
-    </div>
+    </Container>
   );
 }
 export default Resume;
