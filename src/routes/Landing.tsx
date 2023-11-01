cf23a05fe5d720f0ce804fe7df23264ede80a2ef
lalexgap <me@alexgap.ca>
2023-11-01 10:19:19 -0700

    Add some more info

--- src/routes/Landing.tsx
+++ src/routes/Landing.tsx
@@ -9,8 +9,8 @@ function Landing() {
       sx={{ px: 2 }}
     >
       <Box
-        height="86Vh"
         display="flex"
+        maxWidth={800}
         alignItems="center"
         flexDirection="column"
       >
@@ -30,9 +30,26 @@ function Landing() {
         <Typography color="textSecondary" variant="h4" gutterBottom>
           full-stack developer
         </Typography>
-        <Typography color="textPrimary" variant="body1">
-          Check out my <Link href="/resume">resume</Link> or some of the{" "}
-          <Link href="/projects">projects</Link> I've worked on.
+        <Typography color="textPrimary" variant="body1" py={1}>
+          Hi! I'm Alex and I'm a full-stack developer based out of Squamish, BC,
+          Canada. I have over 10 years experience in software development and
+          over 6 years of experience with web3 development. I have experience
+          using a wide range of technologies, including{" "}
+          <Box
+            component="span"
+            sx={(theme) => ({ color: theme.palette.text.secondary })}
+          >
+            React, Typescript, Golang, Solidity
+          </Box>
+          , and I'm always eager to learn new things.
+        </Typography>
+        <Typography color="textPrimary" variant="body1" py={1}>
+          I have a passion for building things learning new ideas. I'm really
+          intersted in web3 techonologies and I even run my own{" "}
+          <Link href="https://www.beaconcha.in/validator/835956f9b6475e055b346c029756b3746a53cfda875b8a1cfe93722b937b93d4ca7334a147201d1673fa0d0c9252d8c9">
+            validator node
+          </Link>
+          .
         </Typography>
       </Box>
     </Box>
