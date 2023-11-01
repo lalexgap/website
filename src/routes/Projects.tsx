23972c53987094818019910666200b953acf8c88
lalexgap <me@alexgap.ca>
2023-11-01 10:19:24 -0700

    reorder projects

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -11,6 +11,24 @@ import {
 function Projects() {
   const projects = [
     {
+      image: "ethglobal-hack.png",
+      content: (
+        <Container>
+          {" "}
+          <Typography gutterBottom variant="h5">
+            <Link href="https://ethglobal.com/showcase/scbridgeaccount-ivyas">
+              ETHGlobal 2023 Hackathon Entry
+            </Link>
+          </Typography>
+          <Typography variant="body2">
+            Me and 3 other team members implemented a smart contract wallet (ERC
+            4337) that also allowed instant bridging of assets between chains,
+            winning multiple prizes.
+          </Typography>
+        </Container>
+      ),
+    },
+    {
       image: "go-nitro.png",
       content: (
         <Container>
@@ -21,7 +39,7 @@ function Projects() {
           </Typography>
           <Typography variant="body2">
             As part of the state channels team I designed and implemented
-            various parts of our state channel framework in Go.
+            various parts of the go-nitro state channels framework.
           </Typography>
         </Container>
       ),
@@ -42,24 +60,7 @@ function Projects() {
         </Container>
       ),
     },
-    {
-      image: "ethglobal-hack.png",
-      content: (
-        <Container>
-          {" "}
-          <Typography gutterBottom variant="h5">
-            <Link href="https://ethglobal.com/showcase/scbridgeaccount-ivyas">
-              Ethglobal 2023 Hackathon Entry
-            </Link>
-          </Typography>
-          <Typography variant="body2">
-            Me and 3 other team members implemented a smart contract wallet (ERC
-            4337) that also allowed instant bridging of assets between chains,
-            winning multiple prizes.
-          </Typography>
-        </Container>
-      ),
-    },
+
     {
       image: "web3torrent.gif",
       content: (
