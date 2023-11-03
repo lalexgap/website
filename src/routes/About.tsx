7bae27f9bf122736211e2655834b5b19212c8455
lalexgap <me@alexgap.ca>
2023-11-03 16:46:33 -0700

    Remove skills and list current job

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,15 +1,6 @@
 import { Box, Typography, Link, CardMedia } from "@mui/material";
 
 function About() {
-  const emphasize = (text: string) => (
-    <Box
-      component="span"
-      sx={(theme) => ({ color: theme.palette.text.secondary })}
-    >
-      {text}
-    </Box>
-  );
-
   return (
     <Box
       display="flex"
@@ -41,20 +32,22 @@ function About() {
         </Typography>
         <Typography color="textPrimary" variant="body1" py={1}>
           Hi! My name is Alex, and I'm a full-stack developer located in
-          Squamish, BC, Canada. With over a decade in software development and
-          more than six years in web3 development, I have honed my skills across
-          a diverse range of technologies, including {emphasize("react")},{" "}
-          {emphasize("typescript")}, {emphasize("golang")}, and{" "}
-          {emphasize("solidity")}.
+          Squamish, BC, Canada. I have over a decade in software development and
+          more than six years in web3 development. I currently work with the{" "}
+          <Link href="https://statechannels.org/team/">
+            State Channels team
+          </Link>{" "}
+          which is funded by{" "}
+          <Link href="https://www.mesh.xyz/">Consensys Mesh</Link>.
         </Typography>
         <Typography color="textPrimary" variant="body1" py={1}>
-          I love building and using open and decentralized technologies. I've{" "}
-          <Link href="https://github.com/lalexgap">contributed</Link> to
-          multiple open source projects and I also run my own ethereum{" "}
+          I love building and using open and decentralized technologies! Check
+          out some of cool the <Link href="/projects">projects</Link> I've
+          worked on. Or check out the etheurem{" "}
           <Link href="https://www.beaconcha.in/validator/835956f9b6475e055b346c029756b3746a53cfda875b8a1cfe93722b937b93d4ca7334a147201d1673fa0d0c9252d8c9">
             validator node
-          </Link>
-          .
+          </Link>{" "}
+          I manage and run.
         </Typography>
       </Box>
     </Box>
